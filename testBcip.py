8489bcbd5c17bee067e518ef623d7778222a3497
Isac De La Cruz <economista.isac@gmail.com>
2023-05-17 11:53:40 -0500

    logueo

--- testBcip.py
+++ testBcip.py
@@ -22,16 +22,19 @@ options.add_experimental_option("prefs", {
     "safebrowsing.enabled": True
 })
 
+#Para Ignorar los errores de certificado SSL (La conexion no es privada)
+options.add_argument("--ignore-certificate-errors")
+
 # Especifica los detalles de la URL, nombre de usuario y contraseña
 url = "https://10.95.224.27:9083/bicp/login.action"
-username = "E8234180X"
-password = "RoboT$n2023X%"
+username = "E8234353"
+password = "JJJmm88***1"
 
 
 # Configura el controlador de Chrome y abre la URL especificada
 driver = webdriver.Chrome(executable_path=pathDriver, options=options)
 driver.get(url)
-
+"""
 # Espera a que se cargue completamente la página web
 driver.implicitly_wait(10)
 
@@ -44,7 +47,10 @@ WebDriverWait(driver, 5)\
                                       'a#proceed-link')))\
     .click()
 
+"""    
 time.sleep(2)
+
+
 #cierra ventana emergente
 elemento_cierre = driver.find_element(By.XPATH, '//*[@id="tipClose"]')
 elemento_cierre.click()
@@ -81,9 +87,43 @@ code = solver.get_result(id)
 code_field = driver.find_element(By.XPATH, '//*[@id="validate"]')
 code_field.send_keys(code)
 
-#presiona boton submit
-submit_button = driver.find_element(By.XPATH, '//*[@id="submitBtn"]/a')
+time.sleep(5)
+#presiona boton submit 
+submit_button = driver.find_element(By.CLASS_NAME, 'login_submit_btn')
 submit_button.click()
+
+time.sleep(5)
+#Menu principal
+systemMenu = driver.find_element(By.ID, 'systemMenu')
+systemMenu.click()
+
+time.sleep(5)
+#selecciona Resource manager del menu principal
+resourceManager = driver.find_element(By.ID, 's_800')
+resourceManager.click()
+time.sleep(5)
+
+#Selecciona searchResource para busqueda de campaña
+searchResource = driver.find_element(By.ID, 'searchResource')
+searchResource.click()
+time.sleep(5)
+
+#Codigo de la campaña
+codigoCampana = '1259'
+
+#input donde se ingresa codigo de la campaña
+inputCampana = driver.find_element(By.ID, 'searchCondtion_input_value')
+inputCampana.send_keys(codigoCampana)
+botonBusqueda = driver.find_element(By.ID, 'searchBtn')
+botonBusqueda.click()
+time.sleep(2)
+
+#selecciona documento
+#gridLink = driver.find_element(By.ID, 'AID_43205')
+#gridLink.click()
+
+
+
 
 time.sleep(10000)
 
@@ -91,41 +131,4 @@ print('termino')
 exit()
 
 
-# Envía el formulario y cierra el controlador de Chrome
-password_field.send_keys(Keys.RETURN)
 
-WebDriverWait(driver, 5)\
-    .until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                      'a#menu-button')))\
-    .click()
-
-menuReporte = driver.find_element(By.XPATH, '//*[@id="sidebar"]/div[1]/div[3]/div[2]/ul/li[3]/a')
-menuReporte.click()
-
-menuReporteNivel2 = driver.find_element(By.XPATH, '//*[@id="sidebar"]/div[1]/div[3]/div[2]/ul/li[3]/ul/li[2]')
-menuReporteNivel2.click()
-
-menuReporteNivel3 = driver.find_element(By.XPATH, '//*[@id="sidebar"]/div[1]/div[3]/div[2]/ul/li[3]/ul/li[2]/ul/li[2]/a')
-menuReporteNivel3.click()
-
-#menuPeriodo = driver.find_element(By.XPATH, '//*[@id="summary-section"]/div[1]/div[1]/div[1]/div[1]/button')
-#menuPeriodo.click()
-
-#menuPeriodoNivel2 = driver.find_element(By.XPATH, '//*[@id="summary-section"]/div[1]/div[1]/div[1]/div[2]/div/ul/li[3]')
-#menuPeriodoNivel2.click()
-
-menuSede = driver.find_element(By.XPATH, '//*[@id="summary-section"]/div[1]/div[2]/div/div[1]/button')
-menuSede.click()
-
-menuSedeNivel2 = driver.find_element(By.XPATH, '//*[@id="summary-section"]/div[1]/div[2]/div/div[2]/div[2]/ul/li[1]')
-menuSedeNivel2.click()
-
-btnDescargar = driver.find_element(By.XPATH, '//*[@id="summary-section"]/div[2]/div/div/button[3]')
-btnDescargar.click()
-
-archivo = driver.find_element(By.CLASS_NAME, 'file-name-span')
-archivo.click()
-
-#driver.close()
-
-time.sleep(10000)
